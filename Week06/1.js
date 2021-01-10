51ee2ed070294cc11cdff3edc5fd59b0d30c533c
Sher-prog <17610588786@163.com>
2021-01-10 20:42:54 +0800

    第六周课程结束

---
+++ Week06/1.js
@@ -0,0 +1,13 @@
+class Dog {
+    constructor () {
+        this.man = 100;
+    }
+    yao () {
+        this.man = this.man - 5;
+    }
+}
+
+let str = "123";
+for (let i = 0; i < str.length; i++) {
+    console.log(str.charCodeAt(i))
+}
